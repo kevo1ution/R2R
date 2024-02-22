23c9b8cd8eff14b471a4646e09b7cc0a62657641
emrgnt-cmplxty <owen@algofi.org>
2024-02-21 21:50:55 -0800

    rollbk config

--- examples/codesearch/search_r2r.py
+++ examples/codesearch/search_r2r.py
@@ -38,45 +38,88 @@ if __name__ == "__main__":
     )
 
 
-    for i, (symbol, extraction) in enumerate(Indexer().extractor()):
-        document_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol))
-        symbol_extraction = f"Symbol: {symbol}\nExtraction:\n\n{extraction}"
-        summary = llm.get_chat_completion(
-            [
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": DESCRIPTION_PROMPT.format(extraction=symbol_extraction)},
+    # for i, (symbol, extraction) in enumerate(Indexer().extractor()):
+    #     document_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol))
+    #     symbol_extraction = f"Symbol: {symbol}\nExtraction:\n\n{extraction}"
+    #     summary = llm.get_chat_completion(
+    #         [
+    #             {"role": "system", "content": "You are a helpful assistant."},
+    #             {"role": "user", "content": DESCRIPTION_PROMPT.format(extraction=symbol_extraction)},
              
-             ],
-            generation_config
-        )
-        description = summary.choices[0].message.content
+    #          ],
+    #         generation_config
+    #     )
+    #     description = summary.choices[0].message.content
 
-        entry_response = client.upsert_entries(
-            [
-                {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-desc-only")),
-                    "blobs": {"txt": description},
-                    "metadata": {"symbol": symbol, 'type': 'desc-only'},
-                },
-                {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-ext-only")),
-                    "blobs": {"txt": extraction},
-                    "metadata": {"symbol": symbol, 'type': 'ext-only'},
-                },
-                {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-symbol-ext-only")),
-                    "blobs": {"txt": symbol_extraction},
-                    "metadata": {"symbol": symbol, 'type': 'symbol-ext-only'},
-                },
-                {
-                    "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-desc-plus-symbol-ext")),
-                    "blobs": {"txt": f"Description:\n{description}\n{symbol_extraction}"},
-                    "metadata": {"symbol": symbol, 'type': 'desc-plus-symbol-ext'},
-                }
-            ],
-            {"embedding_settings": {"do_chunking": "false"}}
-        )
+    #     entry_response = client.upsert_entries(
+    #         [
+    #             {
+    #                 "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-desc-only")),
+    #                 "blobs": {"txt": description},
+    #                 "metadata": {"symbol": symbol, 'type': 'desc-only'},
+    #             },
+    #             {
+    #                 "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-ext-only")),
+    #                 "blobs": {"txt": extraction},
+    #                 "metadata": {"symbol": symbol, 'type': 'ext-only'},
+    #             },
+    #             {
+    #                 "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-symbol-ext-only")),
+    #                 "blobs": {"txt": symbol_extraction},
+    #                 "metadata": {"symbol": symbol, 'type': 'symbol-ext-only'},
+    #             },
+    #             {
+    #                 "document_id": str(uuid.uuid5(uuid.NAMESPACE_DNS, symbol+"-desc-plus-symbol-ext")),
+    #                 "blobs": {"txt": f"Description:\n{description}\n{symbol_extraction}"},
+    #                 "metadata": {"symbol": symbol, 'type': 'desc-plus-symbol-ext'},
+    #             }
+    #         ],
+    #         {"embedding_settings": {"do_chunking": "false"}}
+    #     )
 
-    print("Searching remote db...")
-    search_response = client.search("How does logging take place?", 20)
-    print(f"Search response:\n{search_response}\n\n")
+    # query = "How is logging performed?"
+    # query = "How do we launch a server?"
+    # query = "How do we search?"
+    # query = "How do we embed a document?"
+    # query = "How do we ingest a file?"
+    # query = "How do we create an LLM?"
+    # query = "How do we connect to a vector database?"
+    query = "What are the core pipelines?"
+
+    filters = {"type": "desc-only"}
+    print("-"*25)
+    print(f"Searching remote db with query = {query} and filters={filters}...")
+    search_response = client.search(query, 10, filters=filters)
+    for i, response in enumerate(search_response):
+        metadata = response['metadata']
+        print(f"{i+1}. {metadata['symbol']}: {response['score']:.3f}")
+
+    filters = {"type": "ext-only"}
+    print("-"*25)
+    print(f"Searching remote db with query = {query} and filters={filters}...")
+    search_response = client.search(query, 10, filters=filters)
+    for i, response in enumerate(search_response):
+        metadata = response['metadata']
+        print(f"{i+1}. {metadata['symbol']}: {response['score']:.3f}")
+
+    filters = {"type": "symbol-ext-only"}
+    print("-"*25)
+    print(f"Searching remote db with query = {query} and filters={filters}...")
+    search_response = client.search(query, 10, filters=filters)
+    for i, response in enumerate(search_response):
+        metadata = response['metadata']
+        print(f"{i+1}. {metadata['symbol']}: {response['score']:.3f}")
+
+    filters = {"type": "desc-plus-symbol-ext"}
+    print("-"*25)
+    print(f"Searching remote db with query = {query} and filters={filters}...")
+    search_response = client.search(query, 10, filters=filters)
+    for i, response in enumerate(search_response):
+        metadata = response['metadata']
+        print(f"{i+1}. {metadata['symbol']}: {response['score']:.3f}")
+
+    print("-"*25)
+
+    # print(f"Searching remote db with query = {query} and filters={filters}...")
+    # search_response = client.search(query, 2, filters=filters)
+    # print(f"Leading 2 responses:\n{search_response}\n\n")    
